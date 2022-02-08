9a6926a50a5d7fe926582080b23d26f09d70bd68
mikan <83708535+Ansai940@users.noreply.github.com>
2022-02-08 16:01:22 +0900

    Add remove_duplicate

--- common/trainer.py
+++ common/trainer.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 import sys
 sys.path.append('/Users/ansai/Documents/VScode/deep-learning/deep-learning-from-scratch')
-import numpy
+import numpy as np
 import time
 import matplotlib.pyplot as plt
 
@@ -25,7 +25,7 @@ class Trainer:
         start_time = time.time()
         for epoch in range(max_epoch):
             # シャッフル
-            idx = numpy.random.permutation(numpy.arange(data_size))
+            idx = np.random.permutation(np.arange(data_size))
             x = x[idx]
             t = t[idx]
 
@@ -36,7 +36,8 @@ class Trainer:
                 # 勾配を求め、パラメータを更新
                 loss = model.forward(batch_x, batch_t)
                 model.backward()
-                optimizer.update(model.params, model.grads)
+                params, grads = remove_duplicate(model.params, model.grads)  # 共有された重みを1つに集約
+                optimizer.update(params, grads)
                 total_loss += loss
                 loss_count += 1
 
@@ -52,10 +53,45 @@ class Trainer:
             self.current_epoch += 1
 
     def plot(self, ylim=None):
-        x = numpy.arange(len(self.loss_list))
+        x = np.arange(len(self.loss_list))
         if ylim is not None:
             plt.ylim(*ylim)
         plt.plot(x, self.loss_list, label='train')
         plt.xlabel('iterations (x' + str(self.eval_interval) + ')')
         plt.ylabel('loss')
         plt.show()
+
+
+def remove_duplicate(params, grads):
+    '''
+    パラメータ配列中の重複する重みをひとつに集約し、
+    その重みに対応する勾配を加算する
+    '''
+    params, grads = params[:], grads[:]  # copy list
+
+    while True:
+        find_flg = False
+        L = len(params)
+
+        for i in range(0, L - 1):
+            for j in range(i + 1, L):
+                # 重みを共有する場合
+                if params[i] is params[j]:
+                    grads[i] += grads[j]  # 勾配の加算
+                    find_flg = True
+                    params.pop(j)
+                    grads.pop(j)
+                # 転置行列として重みを共有する場合（weight tying）
+                elif params[i].ndim == 2 and params[j].ndim == 2 and \
+                        params[i].T.shape == params[j].shape and np.all(params[i].T == params[j]):
+                    grads[i] += grads[j].T
+                    find_flg = True
+                    params.pop(j)
+                    grads.pop(j)
+
+                if find_flg: break
+            if find_flg: break
+
+        if not find_flg: break
+
+    return params, grads

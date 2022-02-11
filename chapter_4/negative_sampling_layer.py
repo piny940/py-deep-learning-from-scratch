0be4906a96e6fa6a27f37b8656b712fa353a41ed
mikan <83708535+Ansai940@users.noreply.github.com>
2022-02-11 15:58:31 +0900

    Add NegativeSamplingWithLoss

--- chapter_4/negative_sampling_layer.py
+++ chapter_4/negative_sampling_layer.py
@@ -1,5 +1,8 @@
 import numpy as np
 import collections
+import sys
+sys.path.append('/Users/ansai/Documents/VScode/deep-learning/deep-learning-from-scratch')
+from common.layers import SigmoidWithLoss, EmbeddingDot
 
 
 class UnigramSampler:
@@ -36,3 +39,41 @@ class UnigramSampler:
         
 
         return negative_sample
+
+
+class NegativeSamplingWithLoss:
+    def __init__(self, w, corpus, power=0.75, sample_size=5):
+        self.sample_size = sample_size
+        self.sampler = UnigramSampler(corpus, power, sample_size)
+        self.loss_layers = [SigmoidWithLoss() for _ in range(1 + sample_size)]
+        self.embed_dot_layers = [EmbeddingDot(w) for _ in range(1 + sample_size)]
+        
+        self.params, self.grads = [], []
+        for layers in self.embed_dot_layers:
+            self.params += layers.params
+            self.grads += layers.grads
+        
+    def forward(self, h, target):
+        batch_size = target.shape[0]
+        negative_sample = self.sampler.get_negative_sample(target)
+        
+        # Loss for correct answers
+        score = self.embed_dot_layers[0].forward(h, target)
+        correct_label = np.ones(batch_size, dtype = np.int32)
+        loss = self.loss_layers[0].forward(score, correct_label)
+        
+        # Loss for incorrect answers
+        negative_label = np.zeros(batch_size, dtype=np.int32)
+        for i in range(self.sample_size):
+            negative_target = negative_sample[:, i]
+            score = self.embed_dot_layers[i + 1].forward(h, negative_target)
+            loss += self.loss_layers[i + 1].forward(score, negative_label)
+        
+        return loss
+    
+    def backward(self, dout=1):
+        dh = 0
+        for l0, l1 in zip(self.loss_layers, self.embed_dot_layers):
+            dscore = l0.backward(dout)
+            dh += l1.backward(dscore)
+        return dh

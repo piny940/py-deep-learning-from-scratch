259757c684b401edbf7abf7250cbffec455c95b8
mikan <83708535+Ansai940@users.noreply.github.com>
2022-02-15 12:16:50 +0900

    Add TimeRNN's constructor and forward method

--- common/time_layers.py
+++ common/time_layers.py
@@ -31,3 +31,39 @@ class RNN:
         self.grads[2][...] = db
 
         return dx, dh_prev
+
+
+class TimeRNN:
+    def __init__(self, Wx, Wh, b, stateful=False):
+        self.params = [Wx, Wh, b]
+        self.grads = [np.zeros_like(Wx), np.zeros_like(Wh), np.zeros_like(b)]
+        self.layers = None
+
+        self.h, self.dh = None, None
+        self.stateful = stateful
+    
+    def set_state(self, h):
+        self.h = h
+    
+    def reset_state(self):
+        self.h = None
+    
+    def forward(self, xs):
+        Wx, Wh, b = self.params
+        N, T, D = xs.shape
+        D, H = Wx.shape
+        
+        self.layers = []
+        hs = np.empty((N, T, H), dtype='f')
+        
+        if not self.stateful or self.h == None:
+            self.h = np.zeros((N, H), dtype='f')
+
+        for t in range(T):
+            layer = RNN(*self.params)
+            self.h = layer.forward(xs[:, t, :], self.h)
+            hs[:, t, :] = self.h
+            self.layers.append(layer)
+        
+        return hs
+            

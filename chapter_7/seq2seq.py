9bb9fac50d1317d9332c19e341d3eaf4c11aff0d
mikan <83708535+Ansai940@users.noreply.github.com>
2022-02-25 13:18:46 +0900

    Add Decoder

--- chapter_7/seq2seq.py
+++ chapter_7/seq2seq.py
@@ -1,6 +1,6 @@
 import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-from common.time_layers import TimeEmbedding, TimeLSTM
+from common.time_layers import TimeAffine, TimeEmbedding, TimeLSTM
 import numpy as np
 
 
@@ -34,3 +34,58 @@ class Encoder:
         dout = self.lstm.backward(dhs)
         dout = self.embed.backward(dout)
         return dout
+
+
+class Decoder:
+    def __init__(self, vocab_size, wordvec_size, hidden_size):
+        V, D, H = vocab_size, wordvec_size, hidden_size
+        rn = np.random.randn
+
+        embed_w = (rn(V, D) / 100).astype('f')
+        lstm_wx = (rn(D, 4 * H) / np.sqrt(D)).astype('f')
+        lstm_wh = (rn(H, 4 * H) / np.sqrt(H)).astype('f')
+        lstm_b = np.zeros(4 * H).astype('f')
+        affine_w = (rn(H, V) / np.sqrt(H)).astype('f')
+        affine_b = np.zeros(V).astype('f')
+        
+        self.embed = TimeEmbedding(embed_w)
+        self.lstm = TimeLSTM(lstm_wx, lstm_wh, lstm_b, stateful=True)
+        self.affine = TimeAffine(affine_w, affine_b)
+        
+        self.params, self.grads = [], []
+        
+        for layer in (self.embed, self.lstm, self.affine):
+            self.params += layer.params
+            self.grads += layer.grads
+    
+    def forward(self, xs, h):
+        self.lstm.set_state(h)
+        
+        out = self.embed.forward(xs)
+        out = self.lstm.forward(out)
+        score = self.affine.forward(out)
+        return score
+    
+    def backward(self, dscore):
+        dout = self.affine.backward(dscore)
+        dout = self.lstm.backward(dout)
+        dout = self.embed.backward(dout)
+        
+        dh = self.lsmt.dh
+        return dh
+
+    def generate(self, h, start_id, sample_size):
+        sampled = []
+        sample_id = start_id
+        self.lstm.set_state(h)
+        
+        for _ in range(sample_size):
+            x = np.array(sample_id).reshape((1, 1))
+            out = self.embed.forward(x)
+            out = self.lstm.forward(out)
+            score = self.affine.forward(out)
+            
+            sample_id = np.argmax(score.flatten())
+            sampled.append(int(sample_id))
+        
+        return sampled

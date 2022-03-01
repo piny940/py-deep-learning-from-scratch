c6369dd0a86271d3b7d4baef168a764159d64c5c
mikan <83708535+Ansai940@users.noreply.github.com>
2022-03-01 11:56:54 +0900

    Add AttentionWeight class

--- chapter_8/attention_layer.py
+++ chapter_8/attention_layer.py
@@ -1,4 +1,7 @@
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 import numpy as np
+from common.layers import Softmax
 
 
 class WeightSum:
@@ -27,3 +30,34 @@ class WeightSum:
         da = np.sum(dar, axis=2)
 
         return dhs, da
+
+
+class AttentionWeight:
+    def __init__(self):
+        self.params = []
+        self.grads = []
+        self.softmax = Softmax()
+        self.cache = None
+    
+    def forward(self, hs, h):
+        N, T, H = hs.shape
+        
+        hr = h.reshape(N, 1, H).repeat(T, axis=1)
+        t = hs * hr
+        s = np.sum(t, axis=2)
+        
+        a = self.softmax.forward(s)
+        self.cache = (hs, hr)
+        
+        return a
+    
+    def backward(self, da):
+        hs, hr = self.cache
+        N, T, H = hs.shape
+        
+        ds = self.softmax.backward(da)
+        dt = ds.reshape(N, T, 1).repeat(H, axis=2)
+        dhs = dt * hr
+        dhr = dt * hs
+        dh = np.sum(dhr, axis = 1)
+        return dhs, dh

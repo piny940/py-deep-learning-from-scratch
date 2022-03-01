a1dbea5d243009835fb4f631ff2983a89efce387
mikan <83708535+Ansai940@users.noreply.github.com>
2022-03-02 00:17:52 +0900

    Add TimeAttention class

--- chapter_8/attention_layer.py
+++ chapter_8/attention_layer.py
@@ -81,3 +81,36 @@ class Attention:
         dhs1, dh = self.attention_layer.backward(da)
         dhs = dhs0 + dhs1
         return dhs, dh
+
+
+class TimeAttention:
+    def __init__(self):
+        self.params, self.grads = [], []
+        self.layers = None
+        self.attention_weights = None
+    
+    def forward(self, hs_enc, hs_dec):
+        N, T, H = hs_dec.shape
+        out = np.empty_like(hs_dec)
+        self.layers = []
+        self.attention_weights = []
+        
+        for t in range(T):
+            layer = Attention()
+            out[:, t, :] = layer.forward(hs_enc, hs_dec[N, t, H])
+            self.layers.append(layer)
+            self.attention_weights.append(layer.attention_weight)
+        
+        return out
+    
+    def backward(self, dout):
+        N, T, H = dout.shape
+        dhs_enc = 0
+        dhs_dec = np.empty_like(dout)
+        
+        for t, layer in enumerate(self.layers):
+            dhs, dh = layer.backward(dout[:, t, :])
+            dhs_enc += dhs
+            dhs_dec[:, t, :] = dh
+        
+        return dhs_enc, dhs_dec
